cbe30ba9b1ea7976923d03bdc90245b90a942fca
madhav <madhavnepal916@gmail.com>
2026-01-03 09:08:16 +0545

    configuration of database sqlite

---
+++ app/core/database.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from sqlachemy import create_engine
+from sqlalchemy.orm import sessionmaker,Session
+from sqlalchemy.ext.declarative import declarative_base
+
+from app.core.config import settings
+
+engine = create_engine(
+    settings.DATABASE_URL or "sqlite:///./rag.db",
+    connect_args={"check_same_thread": False} if "sqlite" in (settings.DATABASE_URL or "") else {}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
